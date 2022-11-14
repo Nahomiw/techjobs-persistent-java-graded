9cc15be7fa2b8e6af0b212da1eed62cf408f25ad
Nahomiw <109491375+Nahomiw@users.noreply.github.com>
2022-11-13 22:32:56 -0600

    completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 -- Part 1: Test it with SQL
-
+--COL-> ID DATATYPE->INT
+--COL-> EMPLOYER DATATYPE->VARCHAR
+--COL-> NAME DATATYPE->VARCHAR
+--COL-> SKILLS DATATYPE->VARCHAR
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
-
+DROP Table job;
 -- Part 4: Test it with SQL
+SELECT * from skill join job_skills on skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id is not null
+order by name ASC;
+
+
+
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,42 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    public String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs =  new ArrayList<>();
+
+    public Skill() {
+    }
+
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
 }
